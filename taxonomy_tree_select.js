ade28ed0c24e265b3eb9e0827593ca99dc4567b8
Danillo Nunes <mdanillo@gmail.com>
2010-04-03 00:18:06 +0000

    Now sending the fully functional module

---
+++ taxonomy_tree_select.js
@@ -0,0 +1,112 @@
+// $Id$
+
+Drupal.taxonomyTreeSelect = Drupal.taxonomyTreeSelect ? Drupal.taxonomyTreeSelect : {};
+
+Drupal.behaviors.taxonomyTreeSelect = function(context) {
+  if (vocabularies = Drupal.settings.taxonomyTreeSelect && Drupal.settings.taxonomyTreeSelect) {
+    var $form = $("#node-form", context),
+    $wrapper;
+
+    if ($form.length) {
+      $.each(vocabularies, function(i) {
+        var vocabulary = vocabularies[i];
+        
+        var $wrapper = $("#edit-taxonomy-" + vocabulary.id + "-wrapper");
+        var $select = $("#edit-taxonomy-" + vocabulary.id);
+
+        var listId = "taxonomy-" + vocabulary.id + "-tree";
+        var list = Drupal.taxonomyTreeSelect.BuildList(vocabulary.terms);
+        var classes = vocabulary.classes ? vocabulary.classes : [];
+        classes.push("taxonomy-tree");
+        
+        list = "<div id='" + listId + "' class='" + classes.join(" ") + "'>" + list + "</div>";
+
+        $select.hide();
+        $wrapper.append(list);
+        var $list = $("#" + listId);
+        var $expansables = $list.find("li.expansable");
+
+        $list
+          .bind("click", function(e) {
+            var $label = $(e.target);
+            if (!$label.is("label")) {
+              return;
+            }
+            if (e.ctrlKey && vocabulary.multiple) {
+              var multiple = true;
+            }
+
+            var $item = $label.parent("li");
+            var $child = $label.siblings("ul");
+
+            var labelFor = $label.attr("for");
+
+            if ($child.length && $child.is(":visible") && !multiple) {
+              $item.trigger("collapse");
+            }
+            else if ($child.length && !$child.is(":visible")) {
+              $item.trigger("expand");
+            }
+
+            if (!multiple) {
+              $list.find(".active").removeClass("active");
+              $select.find("option").removeAttr("selected");
+            }
+
+            if (multiple && $label.is(".active")) {
+              $label.removeClass("active");
+              $select.find("option[value='" + labelFor + "']").removeAttr("selected");
+            }
+            else {
+              $label.addClass("active");
+              $select.find("option[value='" + labelFor + "']").attr("selected", "selected");
+            }
+
+            return false;
+          });
+
+        $expansables
+          .bind("collapse", function() {
+            var $item = $(this);
+            var $child = $item.children("ul");
+            $child.slideUp("fast");
+            $item
+              .removeClass("expanded")
+              .addClass("collapsed");
+          })
+          .bind("expand", function() {
+            var $item = $(this);
+            var $child = $item.children("ul");
+            $child.slideDown("fast");
+            $item
+              .removeClass("collapsed")
+              .addClass("expanded");
+          })
+          .children("ul").hide();
+      });
+    }
+  }
+};
+
+Drupal.taxonomyTreeSelect.BuildList = function(terms) {
+  var list = "<ul class='menu'>";
+    $.each(terms, function(i, term) {
+      var classes = [];
+      if (term.terms) {
+        classes.push("collapsed");
+        classes.push("expansable");
+      }
+      else {
+        classes.push("leaf");
+      }
+      list += "<li id='taxonomy-tree-tid-" + term.tid + "' class='" + classes.join(" ") + "'>";
+      list += "<label for='" + term.tid + "' id='edit-taxonomy-tree-tid-" + term.tid + "-link'>" + term.name + "</label>";
+      if (term.terms) {
+        list += Drupal.taxonomyTreeSelect.BuildList(term.terms);
+      }
+      list += "</li>";
+    });
+  list += "</ul>";
+
+  return list;
+};
